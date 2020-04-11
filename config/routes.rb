0115dc10a24309e8b42f4b4cc633decab2272552
TAKERU ARAKAWA <takeruarakawa@TAKERUnoMacBook-Air.local>
2020-04-11 16:29:28 +0900

    [Finish]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get '/' =>'homes#top'
+  root :to  =>'homes#top'
   get '/books' => 'books#index',as: :books
-  get '/books/:id/edit' => 'books#edit'
+  get '/books/:id/edit' => 'books#edit',as: :edit_book
   get '/books/:id' => 'books#show',as: :book
   post '/books' => 'books#create'
-  post '/books/update' => 'books#update'
-  post '/books/:id/destroy' => 'books#destroy'
+  patch '/books/:id' => 'books#update'
+  delete '/books/:id/destroy' => 'books#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
