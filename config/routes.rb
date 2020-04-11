5248b09aca551025944199a472b7a9104e50bd12
vagrant <vagrant@localhost.localdomain>
2020-04-11 10:45:19 +0000

    [Update]4-8修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root :to  =>'homes#top'
-  get '/books' => 'books#index',as: :books
-  get '/books/:id/edit' => 'books#edit',as: :edit_book
-  get '/books/:id' => 'books#show',as: :book
-  post '/books' => 'books#create'
-  patch '/books/:id' => 'books#update'
-  delete '/books/:id/destroy' => 'books#destroy'
+  #get '/books' => 'books#index',as: :books
+  #get '/books/:id/edit' => 'books#edit',as: :edit_book
+  #get '/books/:id' => 'books#show',as: :book
+  #post '/books' => 'books#create'
+  #patch '/books/:id' => 'books#update'
+  #delete '/books/:id/destroy' => 'books#destroy'
+  resources :books
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
